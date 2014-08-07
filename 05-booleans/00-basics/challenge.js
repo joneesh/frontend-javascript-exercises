c93faa40453e06db08c321a58b59ff5fe61cca78
vagrant <vagrant@precise32.(none)>
2014-08-07 12:27:12 +0000

    resolved boolean exercise with mentor

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,21 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
-stringOne == stringTwo;
+return stringOne == stringTwo;
 };
 
 module.exports.notEqual = function(one, two) {
-one != two;
+return one !== two;
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
-lower < middle && middle < upper;
+return lower < middle && middle < upper;
 };
 
 module.exports.outsideRanges = function(number) {
-  number <10 && number >20 && number <= 42 && number >75 && number <=1 && number >= 6;
+return (number < 10 || number > 20) && (number <= 42 || number > 75) && (number <= 1 || number >= 6);
+
 };
 
 module.exports.nameAndPrice = function(name, price) {
-name == 'NYTimes' || name == 'LATimes' && price >= 1;
+//return name == 'NYTimes' || name == 'LATimes' && price >= 1;
+return ((name === 'NYTimes') || (name === 'LATimes')) && price >= 1;
 };
