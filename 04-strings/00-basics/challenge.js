a54922c1d22aeb88cc7424b199bc701b9c7bbfbc
vagrant <vagrant@precise32.(none)>
2014-08-07 12:09:25 +0000

    first submissions 04-06

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,19 +1,26 @@
 module.exports.formLetter = function(firstName, senderName, message) {
-
+console.log("Hello " + firstName + ",\n\n");
+console.log(message+ ".\n\n");
+console.log("Regards,\n" + senderName);
 };
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
-
+var word1 = bigString.substring(startA, endA) ; 
+var word2 = bigString.substring(startB, endB) ; 
+var result = word1 + word2;
+result;
 };
 
 module.exports.findFirstMatch = function(text, searchString) {
-
+text.indexOf(searchString);
 };
 
 module.exports.findLastMatch = function(text, searchString) {
-
+text.lastIndexOf(searchString);
 };
 
 module.exports.substringBetweenMatches = function(text, searchString) {
-
+var word1 = text.indexOf(searchString)
+var word2 = text.lastIndexOf(searchString)
+text.substring(word1, word2);
 };
--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,19 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
-
+stringOne == stringTwo;
 };
 
 module.exports.notEqual = function(one, two) {
-
+one != two;
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
-
+lower < middle && middle < upper;
 };
 
 module.exports.outsideRanges = function(number) {
-
+  number <10 && number >20 && number <= 42 && number >75 && number <=1 && number >= 6;
 };
 
 module.exports.nameAndPrice = function(name, price) {
-
+name == 'NYTimes' || name == 'LATimes' && price >= 1;
 };
