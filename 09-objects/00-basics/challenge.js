0b37205f66956f634a54c775a717179adce348a2
vagrant <vagrant@precise32.(none)>
2014-08-11 12:20:27 +0000

    completed objects exercises-cary

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,36 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle,courseDuration,courseStudents){
+	var courseNew = new Object();
+		 courseNew.title = courseTitle;
+		 courseNew.duration = courseDuration;
+		 courseNew.students = courseStudents;
+		 return courseNew;
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object,newProp,newValue){
+ 	object[newProp] = newValue;
+ 		return object;
+};
 
-module.exports.formLetter = undefined;
+//module.exports.formLetter = function(letter){
+//var newLetter = "[ letter ]";
+//return "Hi" + [newLetter.recipient] + ",\n\n" + [msg] + "Regards,\n" + recipient;
+//};
 
-module.exports.canIGet = undefined;
+module.exports.formLetter = function(x) { 
+	var letter = x; 
+	return 'Hello ' + letter.recipient + ',\n\n' + letter.msg + 
+	'\n\nSincerely,\n' + letter.sender; };
+
+//module.exports.canIGet = function(item, money){
+//	var prices = {
+//        Air : 999,
+//        MacBook: 1299,
+//        Pro: 2499,
+//        Sticker : 1,
+//};
+//};
+
+module.exports.canIGet = function(item, money) { 
+	var products = { 'MacBook Air': 999, 'MacBook Pro': 1299, 
+	'Mac Pro': 2499, 'Apple Sticker': 1 }; 
+	return products[item] <= money; };
