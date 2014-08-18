80e07623edb3d267cc5d6a0fb30c4000106fadae
vagrant <vagrant@precise32.(none)>
2014-08-18 09:24:55 +0000

    loops exercise - cary

--- 10-loops/01-iterating-over-an-object/challenge.js
+++ 10-loops/01-iterating-over-an-object/challenge.js
@@ -1,5 +1,26 @@
-module.exports.getKeys = undefined;
+module.exports.getKeys = function(input){
+var array = [];
+for (var property in input){
+array.push(property); 
+}; 
+return array; 
+};
 
-module.exports.getValues = undefined;
 
-module.exports.objectToArray = undefined;
+module.exports.getValues = function(input){
+var array = [];
+for (var property in input){
+	array.push(input[property]); 
+};
+return array; 
+};
+
+
+module.exports.objectToArray = function(a){
+	var array = [];
+	for (var property in a){
+		 array.push(property +" is "+ a[property]); 
+}
+return array;
+}
+;
